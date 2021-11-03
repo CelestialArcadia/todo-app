e6d681f549649e56eb0c4c6d6eebaa7be264be08
CelestialArcadia <22876185+CelestialArcadia@users.noreply.github.com>
2021-11-03 02:07:07 +0100

    Creation of the repository interface
    "Implementing" CRUD functions.

---
+++ src/main/java/repositories/TodoRepository.java
@@ -0,0 +1,15 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arcadia.todo.model.Todo;
+
+// @Repository annotation marks this interface as a Spring Data JPA repository.
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+	// Through CrudRepository it's possible to use methods save(), findOne(),
+	// findById(), findAll(), count(), delete(), deleteById() without providing
+	// implementation.
+}
